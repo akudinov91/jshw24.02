5c4ff0a3d7df77d91428723ba11cad3611b477cd
akudinov91 <andrewkudinov13@gmail.com>
2024-04-24 20:36:19 +0400

    finished

---
+++ input/main.js
@@ -0,0 +1,24 @@
+// const form = document.querySelector(".form-name");
+// const nameInput = document.querySelector(".username");
+// nameInput.addEventListener("input", (event) => {
+//   const inputText = event.target.value;
+//   let filteredText = "";
+//   for (let i = 0; i < inputText.length; i += 1) {
+//     const char = inputText[i];
+//     if (char >= "0" && char <= "9") {
+//       continue;
+//     }
+//     filteredText += char;
+//   }
+//   nameInput.value = filteredText;
+// });
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const userName = nameInput.value.trim();
+//   if (userName === "") {
+//     alert("Please enter your name.");
+//   } else {
+//     alert(`Glad to see you, ${userName}!`);
+//   }
+// });

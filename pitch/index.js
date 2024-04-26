66e1865e7b3dd5fb7b1a40b22f51746dadeef547
akudinov91 <161059560+akudinov91@users.noreply.github.com>
2024-04-26 17:15:18 +0400

    Full finished

---
+++ pitch/index.js
@@ -0,0 +1,12 @@
+// document.querySelector("#field").addEventListener("click", (event) => {
+//     const ball = document.querySelector("#ball");
+//     const field = document.querySelector("#field");
+//     let mouseX = event.clientX;
+//     let mouseY = event.clientY;
+//     let ballX = mouseX;
+//     let ballY = mouseY;
+  
+//     ball.style.left = ballX + "px";
+//     ball.style.top = ballY + "px";
+//   });
+  
